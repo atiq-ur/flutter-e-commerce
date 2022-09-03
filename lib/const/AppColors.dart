8cc0c8fad5f3472de1aa8b41e3771d8e28eafa3f
Atiqur Rahman <atiq.diu.cse@gmail.com>
2022-09-03 17:48:51 +0600

    add splash screen

---
+++ lib/const/AppColors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color deepOrange = Color(0xFFFF6B6B);
+}
---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/const/AppColors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.deepOrange,
+      body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center, 
+                children: [
+                  Text(
+                    'E-Commerce',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 44.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  const CircularProgressIndicator(color: Colors.white),
+                ]),
+          )),
+    );
+  }
+}
